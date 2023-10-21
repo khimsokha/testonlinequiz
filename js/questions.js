6abbc3d9f666db49b334f5cb5b3f49536f99216b
Khim Sokha <135720320+khimsokha@users.noreply.github.com>
2023-10-21 16:57:13 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "ឈ្មោះក្រូចខាងក្រោម តើក្រូចមួយណាមានទំហំធំជាងគេ?",
+    answer: "ក្រូចខ្លុង",
+    options: [
+      "ក្រូចឆ្មារ",
+      "ក្រូចខ្វិច",
+      "ក្រូចពោធិ៍សាត់",
+      "ក្រូចខ្លុង"
+    ]
+  },
+    {
+    numb: 2,
+    question: "តើអាហារមួយណាជាប្រភេទអាហារការពារ?",
+    answer: "បន្លែ",
+    options: [
+      "ស៊ុត",
+      "សាច់",
+      "បន្លែ",
+      "បាយ"
+    ]
+  },
+    {
+    numb: 3,
+    question: "តើខ្យល់បរិសុទ្ធសំបូរអ្វី?",
+    answer: "អុកស៊ីសែន",
+    options: [
+      "កាបូនិច",
+      "អុកស៊ីសែន",
+      "អ៊ីដ្រូសែន",
+      "ផ្សែង"
+    ]
+  },
+    {
+    numb: 4,
+    question: "តើមួយទស្សវត្សមានប៉ុន្មានឆ្នាំ?",
+    answer: "១០ ឆ្នាំ",
+    options: [
+      "១០០ ឆ្នាំ",
+      "១០០០ ឆ្នាំ",
+      "១០០០០ ឆ្នាំ",
+      "១០ ឆ្នាំ"
+    ]
+  },
+    {
+    numb: 5,
+    question: "អ្នកដែលប្រកបរបរកសិកម្មគេហៅថាអ្វី?",
+    answer: "កសិករ",
+    options: [
+      "កសិករ",
+      "សិប្បករ",
+      "វិចិត្រករ",
+      "វិស្វករ"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
