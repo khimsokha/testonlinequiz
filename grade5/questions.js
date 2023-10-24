abc7b3bc1b4feb71305f0066549a72bb4a2308e8
ឃីម សុខា <khimsokha11f2019@gmail.com>
2023-10-24 16:51:23 +0700

    grade5-6

---
+++ grade5/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "តើសត្វដំរីពពោះប៉ុន្មានខែ?",
+    answer: "២២ខែ",
+    options: [
+      "២១ខែ",
+      "២២ខែ",
+      "២៣ខែ",
+      "២០ខែ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "តើរុក្ខជាតិមួយណាដែលគេប្រើសម្រាប់ធ្វើតម្បាញ?",
+    answer: "ដើមរពាក់",
+    options: [
+      "ដើមរពាក់",
+      "ដើមស្រឡៅ",
+      "ដើមចន្ទ",
+      "ដើមក្រវាញ"
+    ]
+  },
+    {
+    numb: 3,
+    question: "តើដើមដូងប្រេងជាមធ្យមអាចឱ្យផ្លែនៅអាយុប៉ុន្មានឆ្នាំ?",
+    answer: "៣ឆ្នាំ",
+    options: [
+      "៣ឆ្នាំ",
+      "៤ឆ្នាំ",
+      "៥ឆ្នាំ",
+      "២ឆ្នាំ"
+    ]
+  },
+    {
+    numb: 4,
+    question: "តើពាក្យមួយណាដែលមិនមាននៅក្នុងបទ(នគររាជ)?",
+    answer: "សេចក្ដីសុខ",
+    options: [
+      "សេចក្ដីសុខ",
+      "ជ្រោមជ្រែង",
+      "ព្រេងសំណាង",
+      "ជឿជាក់"
+    ]
+  },
+    {
+    numb: 5,
+    question: "តើច្បាប់ស្រី កវីនិពន្ធជាកាព្យអ្វី?",
+    answer: "បទភុជង្គលីលា",
+    options: [
+      "បទកាកគតិ",
+      "បទភុជង្គលីលា",
+      "បទពំនោល",
+      "បទពាក្យប្រាំបី"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ grade6/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "ភាពពេញវ័យរបស់ក្មេងប្រុស និងក្មេងស្រីចាប់ផ្ដើមនៅអាយុប៉ុន្មាន?",
+    answer: "១៥-១៦​ ឆ្នាំ",
+    options: [
+      "១៧-១៨​ ឆ្នាំ",
+      "១៩-២០ ឆ្នាំ",
+      "១២-១៣​ ឆ្នាំ",
+      "១៥-១៦​ ឆ្នាំ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ព្រះមហាក្សត្រអង្គណាដែលគ្រងរាជ្យនៅសម័យចតុមុខ?",
+    answer: "ព្រះបាទពញាយ៉ាត",
+    options: [
+      "ព្រះបាទពញាយ៉ាត",
+      "ព្រះបាទនរោត្តម",
+      "ព្រះបាទអង្គដួង",
+      "ព្រះបាទចន្ទរាជា"
+    ]
+  },
+    {
+    numb: 3,
+    question: "តើបន្ទាយលង្វែកបច្ចុប្បន្នមានទីតាំងស្ថិតនៅឯណា?",
+    answer: "ស្រុកកំពង់ត្រឡាច",
+    options: [
+      "ស្រុកឧត្តុង្គ",
+      "ស្រុកកំពង់ត្រឡាច",
+      "ស្រុកកំពង់ត្របែក",
+      "ស្រុករលាផ្អៀ"
+    ]
+  },
+    {
+    numb: 4,
+    question: "ក្នុងនិមិត្តសញ្ញាគីមីតើ(កាបូន)មានវ៉ាឡង់ប៉ុន្មាន?",
+    answer: "២, ៤",
+    options: [
+      "១",
+      "១, ២",
+      "២, ៤",
+      "៣"
+    ]
+  },
+    {
+    numb: 5,
+    question: "តើនិមិត្តសញ្ញា H<sub>2</sub>O មានន័យដូចម្ដេច?",
+    answer: "ទឹក",
+    options: [
+      "អុកស៊ីសែន",
+      "ខ្យល់",
+      "ទឹក",
+      "កាបូនិច"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
