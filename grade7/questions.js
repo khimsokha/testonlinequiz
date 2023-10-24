a03c113d53265ddcae6ed77b1bcfffda88cda566
ឃីម សុខា <khimsokha11f2019@gmail.com>
2023-10-24 23:50:58 +0700

    grade8

---
+++ grade7/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "ម្ដាយសុខមានកូន ៤នាក់ កូនទី១ឈ្មោះទឹក ទី២ឈ្មោះដី ទី៣ឈ្មោះភ្លើង តើកូនទី៤ឈ្មោះអ្វី?",
+    answer: "សុខ",
+    options: [
+      "ខ្យល់",
+      "សុខ ខ្យល់",
+      "សុខ",
+      "ភ្លើង ខ្យល់"
+    ]
+  },
+    {
+    numb: 2,
+    question: "តើកែវ សារ៉ាត ទទួលមរណភាពនៅប្រទេសណា?",
+    answer: "សហរដ្ឋអាមេរិក",
+    options: [
+      "សហរដ្ឋអាមេរិក",
+      "កាណាដា",
+      "ខ្មែរ",
+      "អូស្ដ្រាលី"
+    ]
+  },
+    {
+    numb: 3,
+    question: "បទ ឆ្នាំអូន៣១ ជាសំនៀងរបស់នរណា?",
+    answer: "ប៉ែន រ៉ន",
+    options: [
+      "រស់ សិរីសុទ្ធ",
+      "ហួយ មាស",
+      "ប៉ែន រ៉ន",
+      "អឹង ណារី"
+    ]
+  },
+    {
+    numb: 4,
+    question: "ក្រឡាផ្ទៃ ចតុកោណកែង?",
+    answer: "បណ្ដោយ X ទទឺង",
+    options: [
+      "បណ្ដោយ x ទទឺង ចែកនឹង ២",
+      "បណ្ដោយ + ទទឺង ចែកនឹង ២",
+      "បណ្ដោយ X ទទឺង",
+      "បណ្ដោយ + ទទឺង"
+    ]
+  },
+    {
+    numb: 5,
+    question: "តើនៅសតវត្សទី៩ អង់គ្លេសបានផលិតម៉ាស៊ីនអ្វី?",
+    answer: "ម៉ាស៊ិនចំហាយទឹក",
+    options: [
+      "ម៉ាស៊ីនកាត់ដេរ",
+      "ម៉ាស៊ីនកិនស្រូវ",
+      "ម៉ាស៊ីនត្រជាក់",
+      "ម៉ាស៊ិនចំហាយទឹក"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ grade8/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "តើក្រុមចលនាតស៊ូរបស់ក្រុម សាឡុត ស ដាក់ឈ្មោះខ្លួនថាដូចម្ដេច?",
+    answer: "បក្សកុម្មុនីស្ដកម្ពុជា",
+    options: [
+      "ខ្មែរក្រម",
+      "បក្សកុម្មុនីស្ដកម្ពុជា",
+      "ខ្មែរស្រឡាញ់ជាតិ",
+      "ខ្មែរឥស្សរៈ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "តើទង់ជាតិសហរដ្ឋអាមេរិកមានផ្កាយប៉ុន្មាន?",
+    answer: "៥០",
+    options: [
+      "៤០",
+      "៣០",
+      "៥០",
+      "២០"
+    ]
+  },
+    {
+    numb: 3,
+    question: "ក្រោយរដ្ឋប្រហារសម្ដេចសីហនុ តើលោកចេង​ ហេងត្រូវបានគេតែងតាំងជាអ្វី?",
+    answer: "ប្រធានរដ្ឋសភា",
+    options: [
+      "ប្រធានព្រឹទ្ធសភា",
+      "ប្រធានរដ្ឋសភា",
+      "ប្រធានាធិបតី",
+      "នាយករដ្ឋមន្រ្ដី"
+    ]
+  },
+    {
+    numb: 4,
+    question: "តើរឿងសូផាត និពន្ធឡើងដោយនរណា?",
+    answer: "រីម គីន",
+    options: [
+      "រីម គីន",
+      "ញ៉ុក ថែម",
+      "គង់ ប៊ុនឈឿន",
+      "ភិក្ខុសោម"
+    ]
+  },
+    {
+    numb: 5,
+    question: "តើលោកស្រី ហ៊ឹម ស៊ីវន មានអាជីពជាអ្វី?",
+    answer: "អ្នកចម្រៀង",
+    options: [
+      "អ្នករបាំ",
+      "អ្នកចម្រៀង",
+      "គ្រូពេទ្យ",
+      "គ្រូបង្រៀន"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
