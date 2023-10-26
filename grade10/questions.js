e825f2b3a4c4a9f05c0c560c7da1e6a25e9275ea
ឃីម សុខា <khimsokha11f2019@gmail.com>
2023-10-26 15:21:46 +0700

    grade10

---
+++ grade10/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "គោលនយោបាយរូបិយវត្ថុ ដាក់ចេញដោយ?",
+    answer: "ធនាគារជាតិ",
+    options: [
+      "ធនាគារជាតិ",
+      "ក្រសួងការពារជាតិ",
+      "រដ្ឋសភាជាតិ",
+      "ក្រសួងពន្ធដារ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ការងារសំខាន់របស់អ្នកដឹកនាំគឺ?",
+    answer: "ចេះបង្កើតនូវចក្ខុវិស័យ",
+    options: [
+      "រៀបចំផែនការឱ្យបានល្អ",
+      "ចេះប្រើពេលវេលា",
+      "ចេះជ្រើសរើសអាទិភាពការងារ",
+      "ចេះបង្កើតនូវចក្ខុវិស័យ"
+    ]
+  },
+    {
+    numb: 3,
+    question: "តើល្បឿនសំឡេងក្នុងទឹកសមុទ្រមានប្រហែលជាប៉ុន្មានក្នុងមួយវិនាទី?",
+    answer: "១៥០០ម៉ែត្រ",
+    options: [
+      "១៥០០ម៉ែត្រ",
+      "១៦០០ម៉ែត្រ",
+      "១៧០០ម៉ែត្រ",
+      "១៨០០ម៉ែត្រ"
+    ]
+  },
+    {
+    numb: 4,
+    question: "តើស៊ីមណូស្ពែមត្រូវប្រើពេលប៉ុន្មានទើបគ្រាប់ទុំ",
+    answer: "២ឆ្នាំ",
+    options: [
+      "១ឆ្នាំ",
+      "២ឆ្នាំ",
+      "៣ឆ្នាំ",
+      "៤ឆ្នាំ"
+    ]
+  },
+    {
+    numb: 5,
+    question: "ក្នុងចំណោមកិរិយាសព្ទទាំងនេះតើ មួយណាជាកិរិយាក្លាយ ?",
+    answer: "បន្ថយ",
+    options: [
+      "រៀន",
+      "វិភាគ",
+      "បន្ថយ",
+      "ពិចារណា"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ grade9/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "តើកម្ពុជាបច្ចុប្បន្នប្រកាន់យករបបគ្រប់គ្រងនយោបាយបែបណា?",
+    answer: "រាជានិយមបែបសភានិយម",
+    options: [
+      "សាធារណរដ្ឋចម្រុះ",
+      "រាជានិយមផ្ដាច់ការ",
+      "កម្មុនីស្ដ",
+      "រាជានិយមបែបសភានិយម"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ភាសាខ្មែរមានដើមកំណើតមកពីណា?",
+    answer: "ពីភាសាខ្មែរ",
+    options: [
+      "ពីភាសាខ្មែរ",
+      "ពីភាសាបាលី",
+      "ពីភាសាសំស្ក្រឹត",
+      "ពីភាសាមន"
+    ]
+  },
+    {
+    numb: 3,
+    question: "អគ្គលេខាធិការអាស៊ានត្រូវបានតែងតាំងដោយ?",
+    answer: "កិច្ចប្រជុំកំពូលអាស៊ាន",
+    options: [
+      "សន្និសីទអាស៊ានស្ដីពីកិច្ចការមុខងារសាធារណៈ",
+      "កិច្ចប្រជុំកំពូលអាស៊ាន",
+      "កិច្ចប្រជុំបច្ចេកទេស",
+      "កិច្ចប្រជុំថ្នាក់ដឹកនាំ"
+    ]
+  },
+    {
+    numb: 4,
+    question: "ការរស់នៅប្រកបដោយអធ្យាស្រ័យ ធម៌សន្ដោស គឺជា",
+    answer: "ទស្សនៈព្រះពុទ្ធ",
+    options: [
+      "ទស្សនៈព្រះពុទ្ធ",
+      "ទស្សនៈខុងជឺ",
+      "ទស្សនៈមហាត្មគន្ធី",
+      "ទស្សនៈឡាហ្វុងតែន"
+    ]
+  },
+    {
+    numb: 5,
+    question: "តើអ្វីដែលជាកត្តាសំខាន់ជម្រុញវិស័យ ICT ?",
+    answer: "ធនធានមនុស្ស",
+    options: [
+      "វិស័យឯកជន",
+      "សម្ភារ និងថវិកា",
+      "ធនធានមនុស្ស",
+      "គ្មានចម្លើយ"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
