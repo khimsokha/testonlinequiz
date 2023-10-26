9ec4b57a6840c243ac5ee1cb53ef22272d33f3a0
ឃីម សុខា <khimsokha11f2019@gmail.com>
2023-10-26 15:59:40 +0700

    grade12

---
+++ grade11/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "មនុស្សដែលដឹងខុសត្រូវស្គាល់ល្អអាក្រក់ ហើយប្រព្រឹត្តតែអំពើល្អតែខ្លួនស្ថិតក្រោមបង្គាប់គេ។ ជននោះគេចាត់ទុកជាមនុស្សមានអ្វី?",
+    answer: "ទំនួលខុសត្រូវ",
+    options: [
+      "ញាណគ្រប់គ្រាន់",
+      "ញាណមិនគ្រប់គ្រាន់",
+      "សិលញាណ",
+      "ទំនួលខុសត្រូវ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ជាទូទៅតើសាប៊ូមាន pH ប៉ុន្មានដែលមិនផ្ដល់ទុក្ខទោសដល់ស្បែក?",
+    answer: "pH 8",
+    options: [
+      "pH 7.5",
+      "pH 8",
+      "pH ខ្ពស់ជាង 8",
+      "pH 7"
+    ]
+  },
+    {
+    numb: 3,
+    question: "តើព្រះពុទ្ធសាសនាបាននាំចូលមកកាន់ប្រទេសកម្ពុជាយើងតាំងពីពេលណា?",
+    answer: "ស.វទី២ នៃគ.ស",
+    options: [
+      "ស.វទី១ មុនគ.ស",
+      "ស.វទី២ មុនគ.ស",
+      "ស.វទី១ នៃគ.ស",
+      "ស.វទី២ នៃគ.ស"
+    ]
+  },
+    {
+    numb: 4,
+    question: "កម្ពុជាចូលជាសមាជិកអាស៊ាននៅឆ្នាំណា?",
+    answer: "១៩៩៩",
+    options: [
+      "១៩៩៨",
+      "១៩៩៩",
+      "២០០០",
+      "១៩៩៧"
+    ]
+  },
+    {
+    numb: 5,
+    question: "បារាំងដាក់អាណានិគមលើខ្មែរជាផ្លូវការ ក្នុងរាជ្យស្ដេចអង្គណា ?",
+    answer: "រាជ្យព្រះបាទនរោត្តម",
+    options: [
+      "រាជ្យព្រះបាទអង្គឌួង",
+      "រាជ្យព្រះបាទនរោត្តមសីហនុ",
+      "រាជ្យព្រះបាទនរោត្តម",
+      "រាជ្យព្រះបាទអង្គចន្ទ"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ grade12/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "មួយណាជាផលវិបាកនៃអាជីវកម្មរ៉ែចំពោះបរិស្ថាន?",
+    answer: "ការស្រុតដី",
+    options: [
+      "ខ្យល់់ព្យុះ",
+      "កង្វះទឹកភ្លៀង",
+      "ភ្លើងឆេះព្រៃ",
+      "ការស្រុតដី"
+    ]
+  },
+    {
+    numb: 2,
+    question: "ភពទាំងអស់ធ្វើចលនាជុំវិញព្រះអាទិត្យក្នុងគន្លងរាងដូចម្ដេច",
+    answer: "រាងអេលីប",
+    options: [
+      "រាងពងមូល",
+      "រាងអេលីប",
+      "រាងចតុកោណកែង",
+      "រាងត្រីកោណ"
+    ]
+  },
+    {
+    numb: 3,
+    question: "តើ(ណៃសត) ជាតួអង្គក្នុងអក្សរសិល្ប៍មួយណា?",
+    answer: "ផ្កាស្រពោន",
+    options: [
+      "ផ្កាស្រពោន",
+      "មាលាដួងចិត្ត",
+      "សូផាត",
+      "ព្រះអាទិត្យថ្មីរះលើផែនដីចាស់"
+    ]
+  },
+    {
+    numb: 4,
+    question: "តើគេចែកអត្រាកម្មជាប៉ុន្មានប្រភេទ?",
+    answer: "៣ប្រភេទ",
+    options: [
+      "២ប្រភេទ",
+      "៤ប្រភេទ",
+      "៦ប្រភេទ",
+      "៣ប្រភេទ"
+    ]
+  },
+    {
+    numb: 5,
+    question: "តើបារាំងប្រគល់ឯករាជ្យឱ្យខ្មែរជាផ្លូវការក្នុងឆ្នាំណា ?",
+    answer: "ឆ្នាំ១៩៥៣",
+    options: [
+      "ឆ្នាំ១៩៥០",
+      "ឆ្នាំ១៩៥១",
+      "ឆ្នាំ១៩៥២",
+      "ឆ្នាំ១៩៥៣"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
